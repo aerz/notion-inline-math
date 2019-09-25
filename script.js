548b29413cce5152b3c23426fd430e16892bc169
Agustín Cisneros <agustincc2@gmail.com>
2019-09-25 15:00:38 +0200

    Add load on start, listeners on click and keyup and some code rework

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name Inline Math for Notion.so
-// @homepageURL https://www.notion.so/evertheylen/Notion-Inline-Math-9c5047a4e7c84643848b3630db8d5a5e
 // @version 0.2.1
 // @match https://www.notion.so/*
 // @grant GM_addStyle
+// @grant GM_setValue
+// @grant GM_getValue
 // @require https://cdn.jsdelivr.net/npm/katex@0.10.0/dist/katex.js
 // ==/UserScript==
 
@@ -13,32 +14,72 @@
 //   - Press F2 to rerender all inline math. You can of course change the shortcut in the code below.
 //   - The inline math will revert to inline code when the block becomes active.
 
-GM_addStyle(`
-.notion-frame span .katex {
-    padding-right: 0 !important;
-}
-`)
+(function inlineMathNotion() {
 
-function rerender_all() {
-    var code = document.querySelectorAll("span[style*=\"monospace\"]")
-    code.forEach(function(el) {
-        var s = el.textContent
-        if (s.startsWith("math:")) {
-            el.style.color = null
-            el.style.background = null
-            s = s.slice(5).trim()
-            console.log("rendering ", s)
-            katex.render(s, el, {throwOnError: false, font: 'mathit'})
-        }
-    })
-}
+  const timerOnFirstLoad = 500
+  const timerOnKeyUp = 5000
+  const timerOnClick = 5000
 
-window.addEventListener('keydown', function(e) {
-    if (e.key == "F2" && !e.ctrlKey && !e.shiftKey && !e.altKey) {
-        console.log("rerender!");
-        rerender_all()
+  GM_addStyle(`
+    .notion-frame span .katex {
+      padding-right: 0 !important;
     }
-}, true)
+  `);
 
-// I don't know a good way to trigger rerender automatically yet
-//document.addEventListener('DOMContentLoaded', rerender_all, false);
+  GM_setValue('firstLoad', false);
+
+  function start() {
+    setListeners()
+    firstLoad()
+  }
+
+  function firstLoad() {
+    const isLoaded = GM_getValue('firstLoad', true)
+
+    if (!isLoaded) {
+      render()
+      setTimeout(firstLoad, timerOnFirstLoad)
+    }
+  }
+
+  function setListeners() {
+    window.addEventListener('keydown', function(e) {
+      if (e.key == "F2" && !e.ctrlKey && !e.shiftKey && !e.altKey) {
+        render()
+      }
+    }, true);
+
+    window.addEventListener('keyup', function(e) {
+      setTimeout(render, timerOnKeyUp)
+    });
+
+    window.addEventListener('click', function(e) {
+      setTimeout(render, timerOnClick)
+    });
+  }
+
+  function render() {
+    const codeBlocks = document.querySelectorAll("span[style*=\"monospace\"]");
+
+    if (codeBlocks.length > 0) {
+      GM_setValue('firstLoad', true);
+    }
+
+    codeBlocks.forEach(codeBlock => {
+      let mathText = codeBlock.textContent
+
+      if (!mathText.startsWith('math:')) {
+        return false
+      }
+
+      codeBlock.style.color = null;
+      codeBlock.style.background = null;
+
+      mathText = mathText.slice(5).trim();
+
+      katex.render(mathText, codeBlock, { throwOnError: false, font: 'mathit' });
+    });
+  }
+
+  return start()
+})();
